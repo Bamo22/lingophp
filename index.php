c68eb64a1b1258d811c1b98c13e599ffc845027c
Rinse M. <therinsem@hotmail.com>
2014-05-01 22:47:39 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,54 @@
+ <?php
+error_reporting(E_ALL ^ E_NOTICE);
+session_start();
+?>
+<html>
+<form method="POST" action="?">
+<input type="text" value="gerard" name="userin" maxlength="6" />
+<input type="submit" value="submit" name="submit" />
+</form>
+<?php
+
+$goedewoord = "koekje";
+// Als de counter nog niet bestaat, vul hem met 0.
+if(empty($_SESSION['counter'])) { $_SESSION['counter'] == 0; }
+$_SESSION['counter']++ ;
+if(!empty($_POST['userin']))
+{
+	if(isset($_SESSION['woord']) || !empty($_SESSION['woord']) || $_SESSION['woord'] != "")
+	{
+		for ($i = 0; $i <= 5; $i++)
+		{
+			$_SESSION['woord'][$_SESSION['counter']][$i]= $_POST['userin']{$i};
+		}
+	}
+	else
+	{
+		for ($i = 0; $i <= 5; $i++)
+		{
+			$_SESSION['woord'][$_SESSION['counter']][$i]= $_POST['userin']{$i};
+			$_SESSION['goedewoord'][$_SESSION['counter']][$i] = $goedewoord{$i};
+		}
+		
+	}
+			for($k = 0; $k <=$_SESSION['counter']; $k++)
+			{
+				for($l = 0; $l <=5; $l++)
+				{
+					if($_SESSION['woord'][$l][$k] == $_SESSION['goedewoord'][2][$l])
+					{
+					echo "Goed gerarden!";
+					}
+				}
+
+			}
+
+		
+	echo "<pre>";
+	print_r($_SESSION['woord']);
+	print_r($_SESSION['goedewoord']);
+	echo "</pre>";
+	if($_POST['userin'] == "delete") { session_destroy(); header("Location: index.php"); }
+}
+?>
+</html>
